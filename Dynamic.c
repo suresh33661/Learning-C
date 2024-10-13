4a88bbd5e8b9d445a5b132fce70890b477332128
suresh33 <sureshpragada3366@gmail.com>
2024-10-13 18:23:52 +0530

    Dynamic-pointers

--- Dynamic.c
+++ Dynamic.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-//#include <iostream>
-#define max 100
+#include <stdlib.h>
 int main(){
-   int a[5] = {1,2,3,4,5};
-   int b[5] = {1,2,3,4,5};
-   int c[5];
-   for (int i = 0; i<5; i++)
-    c[i] = a[i] + b[i];
-    printf("Sum of the arrays\n");
-   
-   for (int i = 0; i<5; i++){
-    printf("%d\n",c[i]);
-   }
-    
+  int i,n,*ptr;
+  printf("Enter no of integers : ");
+  scanf("%d",&n);
+  ptr = (int *) malloc (n * sizeof(int));
+  printf("Enter values :");
+  for (i = 0; i<n ; i++){
+  scanf("%d",(ptr + i));}
+  
+  for (i = 0; i<n; i++){
+  printf("%d",*(ptr + i));
+  printf(" ");
+  }
+  
 }
