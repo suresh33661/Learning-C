37cc0f4e2ba4deb50be8900f0a0916a0d18e429e
suresh33 <sureshpragada3366@gmail.com>
2025-07-09 11:33:26 +0530

    stocks

--- C/DSA/practice/stocks.c
+++ C/DSA/practice/stocks.c
@@ -4,14 +4,10 @@ int main(){
     int price[] = {7,6,4,3,1};
     int len = sizeof(price)/sizeof(price[0]);
 
-    int max_profit = 0;
-    int current = 0;
-
-    for (int i = 0; i<len; i++){
-        for (int j = i+1; j<len; j++){
-            int profit = price[j] - price[i];
-            max_profit = profit > max_profit ? profit : max_profit;
-        }
-    }
-    printf("Maximum profit: %d\n", max_profit);
+   int buy = 0;
+   int sell = 1;i
+   int profit = 0;
+   while (sell < len-1){
+    if(price[buy])
+   }
 }
